8ca45f963388c2a2b9e479bf646bd8d2d42deb5c
Jimi <contact@jimi.sexy>
2022-03-06 21:46:33 +0100

    Fixed contact form submission - Page always reloaded after submission + email input wasn't validated properly

--- src/script.js
+++ src/script.js
@@ -13,31 +13,32 @@ function init() {
 }
 
 function submitContactForm() {
-    let name = document.getElementById('contactName').value;
-    let email = document.getElementById('contactEmail').value;
-    let message = document.getElementById('contactMessage').value;
+    if (document.getElementById('contactName').value !== '' && document.getElementById('contactEmail').value !== '' && document.getElementById('contactMessage').value !== '') {
+        let regex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        if (regex.test(document.getElementById('contactEmail').value)) {
+            fetch('https://jimiswebsite-api.herokuapp.com/contact', {
+                method: 'POST',
+                body: JSON.stringify({
+                    name: document.getElementById('contactName').value,
+                    email: document.getElementById('contactEmail').value,
+                    message: document.getElementById('contactMessage').value
+                })
+            }).then(async (request) => {
+                let response = await request.json();
 
-    if (name !== '' && email !== '' && message !== '') {
-        fetch('https://jimiswebsite-api.herokuapp.com/contact', {
-            method: 'POST',
-            body: JSON.stringify({
-                name: document.getElementById('contactName').value,
-                email: document.getElementById('contactEmail').value,
-                message: document.getElementById('contactMessage').value
-            })
-        }).then(async (request) => {
-            let response = await request.json();
+                if (response.success) {
+                    document.getElementById('contactName').value = '';
+                    document.getElementById('contactEmail').value = '';
+                    document.getElementById('contactMessage').value = '';
 
-            if (response.success) {
-                document.getElementById('contactName').value = '';
-                document.getElementById('contactEmail').value = '';
-                document.getElementById('contactMessage').value = '';
-
-                document.getElementById('contactResponse').innerHTML = '<success>Your contact form has been sent! I will get back to you soon.</success>';
-            } else {
-                console.log(response);
-            }
-        });
+                    document.getElementById('contactResponse').innerHTML = '<success>Your contact form has been sent! I will get back to you soon.</success>';
+                } else {
+                    document.getElementById('contactResponse').innerHTML = '<error>There was an error sending your contact form. Please try again later.</error>';
+                }
+            });
+        } else {
+            document.getElementById('contactResponse').innerHTML = '<error>Please enter a valid email address.</error>';
+        }
     } else {
         document.getElementById('contactResponse').innerHTML = '<error>Please fill out all fields.</error>';
     }
