9ffe930e6c7044d75918002fc5af50667fbd7d95
Jimi <jimi.herk@schule.local>
2024-07-31 00:45:41 +0200

    Added frontpage

---
+++ src/routes/(public)/+layout.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+    import Icon from "@iconify/svelte";
+</script>
+
+<div class="wrapper">
+    <div class="header">
+        <div class="left">
+            <span class="logo">Jimi</span>
+        </div>
+    </div>
+    <div class="content">
+        <slot />
+
+    </div>
+    <div class="footer">
+        <div class="socials">
+            <a href="https://x.com/derjimbob" target="_blank" data-brand="x"><Icon icon="fa6-brands:x-twitter" /></a>
+            <a href="https://github.com/jimiherk" target="_blank" data-brand="github"><Icon icon="fa6-brands:github" /></a>
+            <a href="https://www.linkedin.com/in/jimi-herken/" target="_blank" data-brand="linkedin"><Icon icon="fa6-brands:linkedin-in" /></a>
+        </div>
+        <div class="links">
+            <a href="https://pastebin.com/1Yb1ipZN">Privacy policy</a>
+        </div>
+        <span class="copyright">&#169; 2024, Jimi Herken</span>
+    </div>
+</div>
+
+
+
+<style lang="scss">
+    div.wrapper {
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+
+      div.header {
+        width: 100vw;
+        height: 7.5vh;
+        background-color: black;
+        color: white;
+        display: flex;
+        align-items: center;
+
+        .left {
+          margin-left: 2%;
+          margin-right: auto;
+
+          span.logo {
+            font-family: "Gaegu", sans-serif;
+            font-size: 200%;
+          }
+        }
+      }
+
+      div.content {
+        flex: 1;
+        padding-left: 20%;
+      }
+
+      div.footer {
+        background-color: #f1f1f1;
+        min-height: 150px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-family: "Inter", sans-serif;
+        color: #545454;
+
+        .socials {
+          margin-top: 10px;
+          a {
+            text-decoration: none;
+            color: inherit;
+            font-size: 2rem;
+            margin: 5px;
+            transition: .2s;
+
+            &[data-brand="x"] {
+              &:hover { color: #000 }
+            }
+            &[data-brand="github"] {
+              &:hover { color: #333 }
+            }
+            &[data-brand="linkedin"] {
+              &:hover { color: #0077B5 }
+            }
+          }
+        }
+      }
+    }
+</style>
---
+++ src/routes/(public)/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import Icon from "@iconify/svelte";
+</script>
+
+<h1>Hey there!</h1>
+<p>
+    My name's <span class="name">Jimi</span> and I'm a <Icon icon="flagpack:de" />/<Icon icon="flagpack:se" /> student in Berlin.<br>
+    I do stuff in <span class="js" style="color:#F0DB4FFF">JavaScript</span>, <span class="ts" style="color:#3178C6FF">TypeScript</span> and <span class="rs" style="color:#CE422BFF">Rust</span> among others for fun, for more check out my <a href="https://github.com/jimiherk" target="_blank">GitHub</a> or <a href="mailto:mail@jimiherken.de">send me an E-mail</a>
+</p>
+
+<style lang="scss">
+    h1 {
+      margin-top: 10%;
+      font-size: 4rem;
+      font-family: Inter, "sans-serif";
+      letter-spacing: 0;
+      font-weight: 1000;
+      margin-bottom: 0;
+    }
+
+    p {
+      font-family: Inter, "sans-serif";
+      font-size: 1rem;
+
+      span{
+        &.name {
+          font-family: "Gaegu", sans-serif;
+        }
+
+        &.js, &.ts, &.rs {
+          font-weight: 700;
+        }
+      }
+
+      a {
+        text-decoration: none;
+        color: #0000af;
+        font-weight: 700;
+      }
+    }
+</style>
